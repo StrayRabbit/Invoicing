f8960d710dacb1f98178486aa1767aca0615827c
StrayRabbit <xiaohui_stone@163.com>
2017-06-12 23:11:02 +0800

    销量图添加

--- Invoicing/FormUI/SearchLineChart.Designer.cs
+++ Invoicing/FormUI/SearchLineChart.Designer.cs
@@ -28,10 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.Tickmarks.MinorVisible = false;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Location = new System.Drawing.Point(7, 41);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.View = lineSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(959, 452);
+            this.chartControl1.TabIndex = 0;
+            chartTitle1.Text = "销量走势图";
+            this.chartControl1.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            chartTitle1});
+            // 
+            // SearchLineChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chartControl1);
+            this.Name = "SearchLineChart";
+            this.Size = new System.Drawing.Size(1014, 499);
+            this.Load += new System.EventHandler(this.SearchLineChart_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
     }
 }
--- Invoicing/FormUI/SearchLineChart.cs
+++ Invoicing/FormUI/SearchLineChart.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using DevExpress.XtraCharts;
 
 namespace Invoicing.FormUI
 {
@@ -16,6 +17,56 @@ namespace Invoicing.FormUI
         public SearchLineChart()
         {
             InitializeComponent();
+        }
+
+        private void SearchLineChart_Load(object sender, EventArgs e)
+        {
+            chartControl1.Width = this.Width;
+
+            //DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            //this.chartControl1.Series[0].View = lineSeriesView1; 
+
+            //DevExpress.XtraCharts.StackedBarSeriesView stackedBarSeriesView1 = new DevExpress.XtraCharts.StackedBarSeriesView();
+            //this.chartControl1.Series[0].View = stackedBarSeriesView1;  
+
+            chartControl1.DataSource = GetData_Month(DateTime.Now);
+
+            Series s1 = this.chartControl1.Series[0];//新建一个series类并给控件赋值  
+            s1.ArgumentDataMember = "Date";        //绑定图表的横坐标
+            s1.ValueDataMembers[0] = "Count";        //绑定图表的纵坐标
+            s1.LegendText = "个数";//设置图例文字 就是右上方的小框框   
+        }
+
+        private DataTable GetData_Month(DateTime time)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Count", typeof(int));       //个数
+            dt.Columns.Add("Date", typeof(string));      //日期
+
+            DataRow dr;
+            try
+            {
+                Service.IService.IMobilePhone service = new Service.ServiceImp.MobilePhone();
+                var firstDay = Convert.ToDateTime(time.Year + "-" + time.Month + "-01");
+                var lastDay = Convert.ToDateTime(Convert.ToDateTime(time.AddMonths(1).ToString("yyyy-MM-01")).AddDays(-1).ToString("yyyy-MM-dd 23:59"));
+                var list = service.LoadListAll(p => p.MobileOutTime >= firstDay && p.MobileOutTime < lastDay);
+
+                for (int i = 1; i <= DateTime.DaysInMonth(time.Year, time.Month); i++)
+                {
+                    dr = dt.NewRow();
+                    dr["Date"] = Convert.ToDateTime(time.Year + "-" + time.Month + "-" + i).ToString("yyyy-MM-dd");
+                    dr["Count"] = list.Where(p => p.MobileOutTime >= Convert.ToDateTime(dr["Date"].ToString()) && p.MobileOutTime < Convert.ToDateTime(dr["Date"].ToString() + " 23:59")).ToList().Count;
+                    dt.Rows.Add(dr);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+            return dt;
         }
     }
 }
